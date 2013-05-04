f8dfcbb84e45e281db81a34f7ca4a914109d7914
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-04 12:15:12 -0700

    questions & answers working

--- public/js/application.js
+++ public/js/application.js
@@ -10,18 +10,23 @@ $(document).ready(function() {
   // ADD QUESTION TO NEW SURVEY PAGE
   $('.card_box').on('click', '#add_question', function(e){
     e.preventDefault();
-    var addQuestion = "<div class='question_card'><div class= 'question'> <label class='label' for='Question'>Question<\/label><input type='text' class='new-question' name='question[1][description]' placeholder='What is your question?'><br><\/div><div class='answer'><label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer[1][description]' placeholder='What is your answer?'><br><\/div><input type='button' id='add_answer' value='Add Answer'><\/div>"
+    var addQuestion = "</br><div class='question_card'><div class= 'question'> <label class='label' for='Question'>Question<\/label><input type='text' class='new-question' name='question["+ ($('.question .new-question').length + 1) + "][description]' placeholder='What is your question?'><br><\/div><div class='answers'><div class='answer'><label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer[1][description]' placeholder='What is your answer?'><br><\/div><\/div><input type='button' id='add_answer' value='Add Answer'><\/div>"
     $('.card_box:nth-last-child(1)').append(addQuestion);    
   });
 
   // ADD ANSWER TO NEW SURVEY PAGE
-  $('.card_box').on('click', '.question_card #add_answer', function(e){
+  $('.card_box').on('click', '#add_answer', function(e){
     e.preventDefault();
-    var addAnswer = "<label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer["+ ($('.answer .new-answer').length + 1) + "][description]' placeholder='What is your answer?'><br>"
-    console.log(this);
+    var addAnswer = "<div class='answer'><label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer["+ ($('.answer .new-answer').length + 1) + "][description]' placeholder='What is your answer?'><br><\/div>";
     $($(this).parents()[0]).append(addAnswer);    
-    // $(this)
-    //   .parents()
   });
+// $('.card_box').on('keyup', '#add_answer', function(e){
+//     e.preventDefault();
+//     var addAnswer = "<div class='answer'><label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer["+ ($('.answer .new-answer').length + 1) + "][description]' placeholder='What is your answer?'><br><\/div>";
+//     if (event.keyCode == 13){
+//       $('')
+//     }
+//     $($(this).parents()[0]).append(addAnswer);    
+//   });
 });
 
