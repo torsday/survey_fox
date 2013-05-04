9a3d4a81ac90c0c0eac34bb9cec1b6b6b00eff5d
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-04 11:13:03 -0700

    add answer kinda working

--- public/js/application.js
+++ public/js/application.js
@@ -11,18 +11,17 @@ $(document).ready(function() {
   $('.card_box').on('click', '#add_question', function(e){
     e.preventDefault();
     var addQuestion = "<div class='question_card'><div class= 'question'> <label class='label' for='Question'>Question<\/label><input type='text' class='new-question' name='question[1][description]' placeholder='What is your question?'><br><\/div><div class='answer'><label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer[1][description]' placeholder='What is your answer?'><br><\/div><input type='button' id='add_answer' value='Add Answer'><\/div>"
-    // console.log($($(this).parents()[0]):nth-last-child(2));  
     $('.card_box:nth-last-child(1)').append(addQuestion);    
-    
-    // $($(this).parents()[0]).append(addAnswer);
-
   });
 
   // ADD ANSWER TO NEW SURVEY PAGE
-  $('.question_card').on('click', '#add_answer', function(e){
+  $('.card_box').on('click', '.question_card #add_answer', function(e){
     e.preventDefault();
     var addAnswer = "<label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer["+ ($('.answer .new-answer').length + 1) + "][description]' placeholder='What is your answer?'><br>"
+    console.log(this);
     $($(this).parents()[0]).append(addAnswer);    
+    // $(this)
+    //   .parents()
   });
 });
 
