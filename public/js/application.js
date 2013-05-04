b66b35a184f52cc5329bc2401322a0654a868242
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-04 10:59:59 -0700

    'add question' works

--- public/js/application.js
+++ public/js/application.js
@@ -11,8 +11,11 @@ $(document).ready(function() {
   $('.card_box').on('click', '#add_question', function(e){
     e.preventDefault();
     var addQuestion = "<div class='question_card'><div class= 'question'> <label class='label' for='Question'>Question<\/label><input type='text' class='new-question' name='question[1][description]' placeholder='What is your question?'><br><\/div><div class='answer'><label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer[1][description]' placeholder='What is your answer?'><br><\/div><input type='button' id='add_answer' value='Add Answer'><\/div>"
-    $('.question_card').append(addQuestion);    
-    $($(this).parents()[0]).append(addAnswer);    
+    // console.log($($(this).parents()[0]):nth-last-child(2));  
+    $('.card_box:nth-last-child(1)').append(addQuestion);    
+    
+    // $($(this).parents()[0]).append(addAnswer);
+
   });
 
   // ADD ANSWER TO NEW SURVEY PAGE
