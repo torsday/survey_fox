dea1fd80b48239e122b74006d4009d01d5730635
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-03 22:44:17 -0700

    not working, but meant to be shared

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
+  
+  // LOGOUT
   $('a#sign-out').on("click", function (e) {
     e.preventDefault();
     var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
     request.done(function () { window.location = "/"; });
   });
-  $('#new_survey').on('click', '#add_question', function(e){
+
+  // ADD QUESTION TO NEW SURVEY PAGE
+  $('.card_box').on('click', '#add_question', function(e){
     e.preventDefault();
-    var addQuestion = " <label class=\"label\" for=\"Question\">Question<\/label><input type=\"text\" class=\"new-question\" name=\"question["+ ($('.question .new-question').length + 1) + "][description]\" placeholder=\"What is your question?\"><br>"
-    $('.question').prepend(addQuestion);     
+    var addQuestion = "<div class='question_card'><div class= 'question'> <label class='label' for='Question'>Question<\/label><input type='text' class='new-question' name='question[1][description]' placeholder='What is your question?'><br><\/div><div class='answer'><label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer[1][description]' placeholder='What is your answer?'><br><\/div><input type='button' id='add_answer' value='Add Answer'><\/div>"
+    $('.question_card').append(addQuestion);    
+    $($(this).parents()[0]).append(addAnswer);    
+  });
+
+  // ADD ANSWER TO NEW SURVEY PAGE
+  $('.question_card').on('click', '#add_answer', function(e){
+    e.preventDefault();
+    var addAnswer = "<label class='label' for='Answer'>Answer<\/label><input type='text' class='new-answer' name='answer["+ ($('.answer .new-answer').length + 1) + "][description]' placeholder='What is your answer?'><br>"
+    $($(this).parents()[0]).append(addAnswer);    
   });
 });
 
