b4576264ee1b5984bf48b6e2937bb351fc365d2f
stephenitis <nguyenhstephen@gmail.com>
2013-05-03 18:17:45 -0700

    user can add more questions to their survey

--- public/js/application.js
+++ public/js/application.js
@@ -4,5 +4,10 @@ $(document).ready(function() {
     var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
     request.done(function () { window.location = "/"; });
   });
-
+  $('#new_survey').on('click', '#add_question', function(e){
+    e.preventDefault();
+    var addQuestion = " <label class=\"label\" for=\"Question\">Question<\/label><input type=\"text\" class=\"new-question\" name=\"question["+ ($('.question .new-question').length + 1) + "][description]\" placeholder=\"What is your question?\"><br>"
+    $('.question').prepend(addQuestion);     
+  });
 });
+
